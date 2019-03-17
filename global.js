024c80be770fc74b3e12f40dc4cd52f1c738802b
ujwal dhakal <kevin.ujwal@gmail.com>
2019-03-17 20:00:39 +0545

    stand alone fixes

--- global.js
+++ global.js
@@ -2,6 +2,22 @@ require('babel-register')();
 require('env2')('.env'); // optionally store youre Evironment Variables in .env
 'use strict'
 
-module.exports = {
+var HtmlReporter = require('nightwatch-html-reporter');
+var reporter = new HtmlReporter({
 
+    openBrowser: false,
+    reportFilename: 'index.html',
+    reportsDirectory: __dirname + '/reports'
+
+});
+
+module.exports = {
+    beforeEach: function (browser, done) {
+        require('nightwatch-video-recorder').start(browser, done)
+    },
+    afterEach: function (browser, done) {
+        require('nightwatch-video-recorder').stop(browser, done);
+    },
+    reporter: reporter.fn,
+    custom_assertions_path: ['test/custom-assertions'],
 }
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,9 +1,24 @@
-require('babel-register')()
-require('env2')('.env'); // optionally store youre Evironment Variables in .env
-'use strict'
 
 
 module.exports = (function(settings) {
     settings.test_workers = false;
+
     return settings;
 })(require('./nightwatch.json'));
+
+
+
+
+//
+// var HtmlReporter = require('nightwatch-html-reporter');
+// var reporter = new HtmlReporter({
+//
+//     openBrowser: false,
+//     reportFilename: 'index.html',
+//     reportsDirectory: __dirname + '/reports'
+//
+// });
+//
+// module.exports = {
+//
+// }
